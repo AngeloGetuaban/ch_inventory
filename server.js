b78abb05a653f53b11d7e24729cfdc0371ce046b
AngeloGetuaban <getuaban143@gmail.com>
2024-10-10 10:34:42 +0800

    committed changes
    committed

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port http://localhost:3000');
+const express = require('express');
+const app = express();
+
+// Set up EJS view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Serve static files from 'public'
+app.use(express.static('public'));
+
+// Import admin routes
+const inventoryRoutes = require('./routes/inventory');
+
+// Set up routes
+app.use('/', inventoryRoutes);
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000');
 });
