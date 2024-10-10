4ed88891197320b195786ccf90df533ca2087074
AngeloGetuaban <getuaban143@gmail.com>
2024-10-10 11:17:42 +0800

    changes
    changes

--- api/server.js
+++ api/server.js
@@ -9,16 +9,14 @@ app.set('views', './public');
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Import admin routes
-const inventoryRoutes = require('../routes/inventory');
-
-// Set up routes
-app.use('/', inventoryRoutes);
-
-// Start server
-app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
+// // Start server
+// app.listen(3000, () => {
+//     console.log('Server is running on port http://localhost:3000');
+// });
 
 // Create the HTTP server for Vercel
 module.exports = (req, res) => {
