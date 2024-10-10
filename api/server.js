2c6f776a98302ba6e16b0b067e9e6868b78bae2a
AngeloGetuaban <getuaban143@gmail.com>
2024-10-10 10:43:18 +0800

    another change
    change

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const { createServer } = require('http');
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Import admin routes
+const inventoryRoutes = require('../routes/inventory');
+
+// Set up routes
+app.use('/', inventoryRoutes);
+
+// // Start server
+// app.listen(3000, () => {
+//     console.log('Server is running on port http://localhost:3000');
+// });
+
+// Create the HTTP server for Vercel
+module.exports = (req, res) => {
+    const server = createServer(app);
+    server.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+};
