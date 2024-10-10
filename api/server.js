2fa4c71bf88086df50d68fa41589b1143df66a5b
AngeloGetuaban <getuaban143@gmail.com>
2024-10-10 11:36:37 +0800

    move to render
    move to render

--- api/server.js
+++ api/server.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const app = express();
-const { createServer } = require('http');
 
-// Set EJS as the view engine
+// Set up EJS view engine
 app.set('view engine', 'ejs');
-app.set('views', './public');
+app.set('public', './public');
 
-// Serve static files from the 'public' directory
+// Serve static files from 'public'
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+// Import admin routes
+const inventoryRoutes = require('../routes/inventory');
+
+// Set up routes
+app.use('/', inventoryRoutes);
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000');
 });
 
-// // Start server
-// app.listen(3000, () => {
-//     console.log('Server is running on port http://localhost:3000');
-// });
-
-// Create the HTTP server for Vercel
-module.exports = (req, res) => {
-    const server = createServer(app);
-    server.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-};
